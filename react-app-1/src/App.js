a4a321cee6dafa0d944f2ee42fb8821c3578cffe
Tyler Sullberg <tysullberg@gmail.com>
2021-03-04 12:35:50 -0800

    xxx

--- react-app-1/src/App.js
+++ react-app-1/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Sup <code>src/App.js</code> and save to reload.
+          Sup yo <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
