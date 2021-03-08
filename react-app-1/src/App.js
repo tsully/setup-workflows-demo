7e82eb9f9726c65d79d1e37089f5d54404d8bb9a
Tyler Sullberg <tysullberg@gmail.com>
2021-03-08 10:01:54 -0800

    Fix failing tests.

--- react-app-1/src/App.js
+++ react-app-1/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is react-app-1.<3
+          Howdy! This is react-app-1. 
         </p>
         <a
           className="App-link"
