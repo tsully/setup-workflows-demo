d5e869ab1329d2a3e03dc5368f76028cbea8709d
Tyler Sullberg <tysullberg@gmail.com>
2021-03-09 10:20:17 -0800

    xxx

--- react-app-1/src/App.js
+++ react-app-1/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Howdy! This is react-app-1. 
+          Hi! This is react-app-1. 
         </p>
         <a
           className="App-link"
